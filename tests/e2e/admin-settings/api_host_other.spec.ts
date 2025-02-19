4fc69b626c2d912fcc00a4d51979e5c7c201789b
costdev <costdev@git.wordpress.org>
2025-02-19 22:41:19 +0000

    E2E: Add tests to ensure protocols are stripped, except HTTP.

--- tests/e2e/admin-settings/api_host_other.spec.ts
+++ tests/e2e/admin-settings/api_host_other.spec.ts
@@ -31,4 +31,84 @@ test.describe('[Admin Settings] [Field] API Host Other', () => {
 		await expect(page.locator(fieldSelector + api_host_other)).toBeVisible();
 		await expect(page.locator(fieldSelector + api_host_other)).toHaveValue('');
 	});
+
+	test('strips an HTTPS protocol', async ({ page }) => {
+		await page.check(fieldSelector + enable);
+		await page.selectOption(fieldSelector + api_host, 'other' );
+		await expect(page.locator(fieldSelector + api_host_other)).toBeVisible();
+		await page.fill(fieldSelector + api_host_other, 'https://my.api.org');
+		await page.locator(fieldSelector + api_host_other).evaluate(e => e.blur());
+		await page.waitForTimeout(250);
+		await expect(page.locator(fieldSelector + api_host_other)).toHaveValue('my.api.org');
+	});
+
+	test('strips an FTP protocol', async ({ page }) => {
+		await page.check(fieldSelector + enable);
+		await page.selectOption(fieldSelector + api_host, 'other' );
+		await expect(page.locator(fieldSelector + api_host_other)).toBeVisible();
+		await page.fill(fieldSelector + api_host_other, 'ftp://my.api.org');
+		await page.locator(fieldSelector + api_host_other).evaluate(e => e.blur());
+		await page.waitForTimeout(250);
+		await expect(page.locator(fieldSelector + api_host_other)).toHaveValue('my.api.org');
+	});
+
+	test('strips an sFTP protocol', async ({ page }) => {
+		await page.check(fieldSelector + enable);
+		await page.selectOption(fieldSelector + api_host, 'other' );
+		await expect(page.locator(fieldSelector + api_host_other)).toBeVisible();
+		await page.fill(fieldSelector + api_host_other, 'sftp://my.api.org');
+		await page.locator(fieldSelector + api_host_other).evaluate(e => e.blur());
+		await page.waitForTimeout(250);
+		await expect(page.locator(fieldSelector + api_host_other)).toHaveValue('my.api.org');
+	});
+
+	test('strips an FTPs protocol', async ({ page }) => {
+		await page.check(fieldSelector + enable);
+		await page.selectOption(fieldSelector + api_host, 'other' );
+		await expect(page.locator(fieldSelector + api_host_other)).toBeVisible();
+		await page.fill(fieldSelector + api_host_other, 'ftps://my.api.org');
+		await page.locator(fieldSelector + api_host_other).evaluate(e => e.blur());
+		await page.waitForTimeout(250);
+		await expect(page.locator(fieldSelector + api_host_other)).toHaveValue('my.api.org');
+	});
+
+	test('strips an SMTP protocol', async ({ page }) => {
+		await page.check(fieldSelector + enable);
+		await page.selectOption(fieldSelector + api_host, 'other' );
+		await expect(page.locator(fieldSelector + api_host_other)).toBeVisible();
+		await page.fill(fieldSelector + api_host_other, 'smtp://my.api.org');
+		await page.locator(fieldSelector + api_host_other).evaluate(e => e.blur());
+		await page.waitForTimeout(250);
+		await expect(page.locator(fieldSelector + api_host_other)).toHaveValue('my.api.org');
+	});
+
+	test('strips a file protocol', async ({ page }) => {
+		await page.check(fieldSelector + enable);
+		await page.selectOption(fieldSelector + api_host, 'other' );
+		await expect(page.locator(fieldSelector + api_host_other)).toBeVisible();
+		await page.fill(fieldSelector + api_host_other, 'file:///my.api.org');
+		await page.locator(fieldSelector + api_host_other).evaluate(e => e.blur());
+		await page.waitForTimeout(250);
+		await expect(page.locator(fieldSelector + api_host_other)).toHaveValue('my.api.org');
+	});
+
+	test('strips a malformed HTTPS protocol', async ({ page }) => {
+		await page.check(fieldSelector + enable);
+		await page.selectOption(fieldSelector + api_host, 'other' );
+		await expect(page.locator(fieldSelector + api_host_other)).toBeVisible();
+		await page.fill(fieldSelector + api_host_other, 'httpa://my.api.org');
+		await page.locator(fieldSelector + api_host_other).evaluate(e => e.blur());
+		await page.waitForTimeout(250);
+		await expect(page.locator(fieldSelector + api_host_other)).toHaveValue('my.api.org');
+	});
+
+	test('respects an HTTP protocol', async ({ page }) => {
+		await page.check(fieldSelector + enable);
+		await page.selectOption(fieldSelector + api_host, 'other' );
+		await expect(page.locator(fieldSelector + api_host_other)).toBeVisible();
+		await page.fill(fieldSelector + api_host_other, 'http://my.api.org');
+		await page.locator(fieldSelector + api_host_other).evaluate(e => e.blur());
+		await page.waitForTimeout(250);
+		await expect(page.locator(fieldSelector + api_host_other)).toHaveValue('http://my.api.org');
+	});
 })
