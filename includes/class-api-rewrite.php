c5c483c36ce2e0832fc6e22a0e216f7090cfde7e
costdev <costdev@git.wordpress.org>
2025-02-21 05:19:08 +0000

    API_Rewrite: Simplify logic in `::pre_http_request()`.
    Simplifies some conditions.
    Removes some unnecessary conditions.
    Adds early returns.

--- includes/class-api-rewrite.php
+++ includes/class-api-rewrite.php
@@ -117,77 +117,68 @@ class API_Rewrite {
 	 * @return mixed The response or false.
 	 */
 	public function pre_http_request( $response, $parsed_args, $url ) {
-		if (
-			isset( $this->default_host ) &&
-			( '' !== $this->default_host ) &&
-			isset( $this->redirected_host ) &&
-			( '' !== $this->redirected_host )
-		) {
-			if ( false !== strpos( $url, $this->default_host ) ) {
-				Debug::log_string( __( 'Default API Found: ', 'aspireupdate' ) . $url );
-
-				if ( false === filter_var( $this->redirected_host, FILTER_VALIDATE_URL ) ) {
-					$error_message = __( 'Your API host is not a valid URL.', 'aspireupdate' );
-					Debug::log_string(
-						sprintf(
-							/* translators: %s: The error message. */
-							__( 'Request Failed: %s', 'aspireupdate' ),
-							$error_message
-						)
-					);
-					return new \WP_Error( 'invalid_host', $error_message );
-				}
-
-				if ( $this->default_host !== $this->redirected_host ) {
-					if ( $this->disable_ssl ) {
-						Debug::log_string( __( 'SSL Verification Disabled', 'aspireupdate' ) );
-						$parsed_args['sslverify'] = false;
-					}
-
-					$parsed_args = $this->add_authorization_header( $parsed_args );
-					$parsed_args = $this->add_accept_json_header( $parsed_args, $url );
-					$url         = $this->add_cache_buster( $url );
-					$protocol    = wp_parse_url( $url, PHP_URL_SCHEME );
-					$updated_url = str_replace(
-						"{$protocol}://{$this->default_host}",
-						untrailingslashit( $this->redirected_host ),
-						$url
-					);
-					Debug::log_string( __( 'API Rerouted to: ', 'aspireupdate' ) . $updated_url );
-
-					Debug::log_request( $parsed_args );
-
-					/**
-					 * Temporarily Unhook Filter to prevent recursion.
-					 */
-					remove_filter( 'pre_http_request', [ $this, 'pre_http_request' ] );
-					$response = wp_remote_request( $updated_url, $parsed_args );
-					add_filter( 'pre_http_request', [ $this, 'pre_http_request' ], 10, 3 );
-
-					Debug::log_response( $response );
-
-					$response_code         = wp_remote_retrieve_response_code( $response );
-					$codes_to_pass_through = [
-						200,
-						404,
-					];
-					if ( ! in_array( $response_code, $codes_to_pass_through, true ) ) {
-						$message = wp_remote_retrieve_response_message( $response );
-						Debug::log_string(
-							sprintf(
-								/* translators: %s: The response message. */
-								__( 'Request Failed: %s', 'aspireupdate' ),
-								$message
-							)
-						);
-						return new \WP_Error( 'failed_request', $message );
-					}
-
-					return $response;
-
-				}
-			}
+		if ( '' === $this->redirected_host || false === strpos( $url, $this->default_host ) ) {
+			return $response;
 		}
+
+		Debug::log_string( __( 'Default API Found: ', 'aspireupdate' ) . $url );
+
+		if ( $this->default_host === $this->redirected_host ) {
+			return $response;
+		}
+
+		if ( false === filter_var( $this->redirected_host, FILTER_VALIDATE_URL ) ) {
+			$error_message = __( 'Your API host is not a valid URL.', 'aspireupdate' );
+			Debug::log_string(
+				sprintf(
+					/* translators: %s: The error message. */
+					__( 'Request Failed: %s', 'aspireupdate' ),
+					$error_message
+				)
+			);
+			return new \WP_Error( 'invalid_host', $error_message );
+		}
+
+		if ( $this->disable_ssl ) {
+			Debug::log_string( __( 'SSL Verification Disabled', 'aspireupdate' ) );
+			$parsed_args['sslverify'] = false;
+		}
+
+		$parsed_args = $this->add_authorization_header( $parsed_args );
+		$parsed_args = $this->add_accept_json_header( $parsed_args, $url );
+		$url         = $this->add_cache_buster( $url );
+		$protocol    = wp_parse_url( $url, PHP_URL_SCHEME );
+		$updated_url = str_replace(
+			"{$protocol}://{$this->default_host}",
+			untrailingslashit( $this->redirected_host ),
+			$url
+		);
+
+		Debug::log_string( __( 'API Rerouted to: ', 'aspireupdate' ) . $updated_url );
+		Debug::log_request( $parsed_args );
+
+		// Temporarily unhook filter to prevent recursion.
+		remove_filter( 'pre_http_request', [ $this, 'pre_http_request' ] );
+		$response = wp_remote_request( $updated_url, $parsed_args );
+		add_filter( 'pre_http_request', [ $this, 'pre_http_request' ], 10, 3 );
+
+		Debug::log_response( $response );
+
+		$response_code         = wp_remote_retrieve_response_code( $response );
+		$codes_to_pass_through = [ 200, 404 ];
+
+		if ( ! in_array( $response_code, $codes_to_pass_through, true ) ) {
+			$message = wp_remote_retrieve_response_message( $response );
+			Debug::log_string(
+				sprintf(
+					/* translators: %s: The response message. */
+					__( 'Request Failed: %s', 'aspireupdate' ),
+					$message
+				)
+			);
+			return new \WP_Error( 'failed_request', $message );
+		}
+
 		return $response;
 	}
 }
